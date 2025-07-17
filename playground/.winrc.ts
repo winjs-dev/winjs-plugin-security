75eee5d157fb193b2d678b8ad3e94e9b5bdb148b
liwb <lwbhtml@163.com>
2025-07-17 10:17:09 +0800

    chore(security): 注释掉 SRI 配置并添加算法选项

--- playground/.winrc.ts
+++ playground/.winrc.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'win';
 export default defineConfig({
   plugins: ['../src'],
   security: {
-    sri: true,
+    // sri: true,
+    sri: {
+      // algorithm: 'sha256',
+      // algorithm: 'sha384',
+    },
   },
 });
